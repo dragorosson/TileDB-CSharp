63a5ad5d7a5823d1b7084b5fa5aa9dbeb967a7ba
Drago Rosson <drago@rosson.me>
2022-12-27 21:21:58 -0600

    testing

--- examples/TileDB.CSharp.Example/ExampleQuery.cs
+++ examples/TileDB.CSharp.Example/ExampleQuery.cs
@@ -1,92 +1,171 @@
-using System;
+//using System;
+using Microsoft.VisualBasic;
 using System.IO;
 using TileDB.CSharp;
-namespace TileDB.CSharp.Examples
+using TileDB.CSharp.Examples;
+namespace Examples
 {
-    static class ExampleQuery
+    class ExampleQuery
     {
-        private static readonly string ArrayPath = ExampleUtil.MakeExamplePath("sparse-array");
-        private static readonly Context Ctx = Context.GetDefault();
+        //private static readonly string arrayUri = $"gcs://drago-deps-bucket/array{System.Guid.NewGuid()}";
+        private static readonly string arrayUri = ExampleUtil.MakeTempPath($"array{System.Guid.NewGuid()}");
 
-        private static void CreateArray()
+        private const int CellVal = 1;
+        protected static void CreateArray()
         {
+            var context = Context.GetDefault();
+            context.Config().Set("vfs.gcs.project_id", "edxt-dev-test");
             // Create array
-            int[] dim1_bound = new int[] { 1, 4 };
-            int dim1_extent = 4;
-            var dim1 = Dimension.Create<int>(Ctx, "rows", dim1_bound, dim1_extent);
-            int[] dim2_bound = new int[] { 1, 4 };
-            int dim2_extent = 4;
-            var dim2 = Dimension.Create<int>(Ctx, "cols", dim2_bound, dim2_extent);
-            var domain = new Domain(Ctx);
+            int[] dim1_bound = new int[] { 0, 2 };
+            int dim1_extent = 3;
+            var dim1 = Dimension.Create<int>(context, "rows", dim1_bound, dim1_extent);
+            
+            int[] dim2_bound = new int[] { 0, 2 };
+            int dim2_extent = 3;
+            var dim2 = Dimension.Create<int>(context, "cols", dim2_bound, dim2_extent);
+
+            var domain = new Domain(context);
             domain.AddDimension(dim1);
             domain.AddDimension(dim2);
-            var array_schema = new ArraySchema(Ctx, ArrayType.Sparse);
-            var attr = new Attribute(Ctx, "a", DataType.Int32);
+
+            var array_schema = new ArraySchema(context, ArrayType.Dense);
+
+            var attr = new Attribute(context, "a", DataType.Int32);
+            attr.SetCellValNum(CellVal);
+            attr.SetFillValue(-7);
+
             array_schema.AddAttribute(attr);
             array_schema.SetDomain(domain);
             array_schema.Check();
+            /*
+            var tmpArrayPath = Path.Join(Path.GetTempPath(), "tiledb_example_sparse_array");
 
-            Array.Create(Ctx, ArrayPath, array_schema);
+            if (Directory.Exists(tmpArrayPath))
+            {
+                Directory.Delete(tmpArrayPath, true);
+            }
+            */
 
-        } //private void CreateArray()
+            Array.Create(context, arrayUri, array_schema);
 
-        private static void WriteArray()
+        } //protected void CreateArray()
+
+        protected static void WriteArray()
         {
-            var dim1_data_buffer = new int[3] { 1, 2, 3 };
-            var dim2_data_buffer = new int[3] { 1, 3, 4 };
-            var attr_data_buffer = new int[3] { 1, 2, 3 };
+            WriteArray1(new int[4] { 0, 1, 0, 1 }, 0);
+            ReadArray();
+            WriteArray1(new int[4] { 1, 2, 0, 1 }, 1);
+            ReadArray();
+            WriteArray1(new int[4] { 0, 1, 1, 2 }, 2);
+            ReadArray();
+            WriteArray1(new int[4] { 1, 2, 1, 2 }, 3);
+        }
+        protected static void WriteArray1(int[] subarray, int val)
+        {
+            var context = Context.GetDefault();
 
-            using (var array_write = new Array(Ctx, ArrayPath))
+            var attr_data_buffer = new int[CellVal * 4];
+
+            for (int i = 0; i < attr_data_buffer.Length; i++)
+            {
+                attr_data_buffer[i] = val;
+            }
+
+            /*
+            var attr_data_buffer = new int[]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+            };
+            */
+
+            /*
+            var attr_data_buffer = new int[]
+            {
+                0, 1, 2, 3, 4,
+            };
+            */
+
+            // NOTE: Subarray indices here are { rowMin, rowMax, colMin, colMax }
+            // not { rowMin, colMin, rowMax, colMax }!
+            //var subarray = new int[4] { 0, 1, 0, 1 };
+            using (var array_write = new Array(context, arrayUri))
             {
                 array_write.Open(QueryType.Write);
-                var query_write = new Query(Ctx, array_write);
-                query_write.SetLayout(LayoutType.Unordered);
-                query_write.SetDataBuffer<int>("rows", dim1_data_buffer);
-                query_write.SetDataBuffer<int>("cols", dim2_data_buffer);
-                query_write.SetDataBuffer<int>("a", attr_data_buffer);
+                var query_write = new Query(context, array_write);
+                query_write.SetLayout(LayoutType.RowMajor);
+                query_write.SetSubarray(subarray);
+                query_write.SetDataBuffer("a", attr_data_buffer);
                 query_write.Submit();
+                var status = query_write.Status();
                 array_write.Close();
             }//array_write
 
         }
 
-        private static void ReadArray()
+        public static void ConsolidateArray()
         {
-            var dim1_data_buffer_read = new int[3];
-            var dim2_data_buffer_read = new int[3];
-            var attr_data_buffer_read = new int[3];
+            System.DateTime start;
+            System.Console.WriteLine($"Consolidating meta...");
+            start = System.DateTime.Now;
+            Context.GetDefault().Config().Set("sm.consolidation.mode", "fragment_meta");
+            Array.Consolidate(Context.GetDefault(), arrayUri, Context.GetDefault().Config());
+            System.Console.WriteLine($"{(System.DateTime.Now - start).TotalMilliseconds}ms Consolidated.");
 
-            using (var array_read = new Array(Ctx, ArrayPath))
+            System.Console.WriteLine($"Consolidating commits...");
+            start = System.DateTime.Now;
+            Context.GetDefault().Config().Set("sm.consolidation.mode", "commits");
+            Array.Consolidate(Context.GetDefault(), arrayUri, Context.GetDefault().Config());
+            System.Console.WriteLine($"{(System.DateTime.Now - start).TotalMilliseconds}ms Consolidated.");
+
+            System.Console.WriteLine($"Vacuuming...");
+            start = System.DateTime.Now;
+            Array.Vacuum(Context.GetDefault(), arrayUri);
+            System.Console.WriteLine($"{(System.DateTime.Now - start).TotalMilliseconds}ms Vacuumed.");
+        }
+
+        protected static void ReadArray()
+        {
+            var context = Context.GetDefault();
+            var tmpArrayPath = Path.Join(Path.GetTempPath(), "tiledb_example_sparse_array");
+            //var attr_data_buffer_read = new int[40];
+            var attr_data_buffer_read = new int[CellVal * 9];
+
+            var subarray = new int[4] { 0, 2, 0, 2 };
+
+            using (var array_read = new Array(context, arrayUri))
             {
                 array_read.Open(QueryType.Read);
-                var query_read = new Query(Ctx, array_read);
+                var query_read = new Query(context, array_read);
                 query_read.SetLayout(LayoutType.RowMajor);
-                query_read.SetDataBuffer<int>("rows", dim1_data_buffer_read);
-                query_read.SetDataBuffer<int>("cols", dim2_data_buffer_read);
+                query_read.SetSubarray(subarray);
                 query_read.SetDataBuffer<int>("a", attr_data_buffer_read);
                 query_read.Submit();
+                var status_read = query_read.Status();
                 array_read.Close();
             }
 
-            System.Console.WriteLine("dim1:{0},{1},{2}", dim1_data_buffer_read[0], dim1_data_buffer_read[1], dim1_data_buffer_read[2]);
-            System.Console.WriteLine("dim2:{0},{1},{2}", dim2_data_buffer_read[0], dim2_data_buffer_read[1], dim2_data_buffer_read[2]);
-            System.Console.WriteLine("attr:{0},{1},{2}", attr_data_buffer_read[0], attr_data_buffer_read[1], attr_data_buffer_read[2]);
+            System.Console.WriteLine($"READ: {string.Join(", ", attr_data_buffer_read)}");
         }
 
-        private static void OnReadCompleted(object sender, QueryEventArgs args)
+        protected static void OnReadCompleted(object sender, QueryEventArgs args)
         {
             System.Console.WriteLine("Read completed!");
         }
 
-        private static void ReadArrayAsync()
+        protected static void ReadArrayAsync()
         {
+            var context = Context.GetDefault();
+            var tmpArrayPath = Path.Join(Path.GetTempPath(), "tiledb_example_sparse_array");
             var dim1_data_buffer_read = new int[3];
             var dim2_data_buffer_read = new int[3];
             var attr_data_buffer_read = new int[3];
 
-            var array_read = new Array(Ctx, ArrayPath);
+            var array_read = new Array(context, arrayUri);
             array_read.Open(QueryType.Read);
-            var query_read = new Query(Ctx, array_read);
+            var query_read = new Query(context, array_read);
             query_read.SetLayout(LayoutType.RowMajor);
             query_read.SetDataBuffer<int>("rows", dim1_data_buffer_read);
             query_read.SetDataBuffer<int>("cols", dim2_data_buffer_read);
@@ -95,17 +174,63 @@ namespace TileDB.CSharp.Examples
             query_read.SubmitAsync();
         }
 
-        public static void Run()
+        public static void ReadFragments()
         {
-            if (Directory.Exists(ArrayPath))
+            using var fi = new FragmentInfo(Context.GetDefault(), arrayUri);
+            fi.Load();
+            System.Console.WriteLine($"Fragments: {fi.FragmentCount}");
+            System.Console.WriteLine($"Cells: {fi.TotalCellCount}");
+            System.Console.WriteLine($"Unconsolidated meta: {fi.FragmentWithUnconsolidatedMetadataCount}");
+            System.Console.WriteLine($"Unvacuumed: {fi.FragmentToVacuumCount}");
+
+            for (uint i = 0; i < fi.FragmentCount; i++)
             {
-                Directory.Delete(ArrayPath, true);
+                System.Console.WriteLine();
+                System.Console.WriteLine($"Fragment {i}:");
+                System.Console.WriteLine($"Size {fi.GetFragmentSize(i)}");
+                System.Console.WriteLine($"Domain0 {fi.GetNonEmptyDomain<int>(i, 0)}");
+                System.Console.WriteLine($"Domain1 {fi.GetNonEmptyDomain<int>(i, 1)}");
+                (ulong start, ulong end) = fi.GetTimestampRange(i);
+                ReadFragmentData(start, end);
+            }
+        }
+
+        public static void ReadFragmentData(ulong start, ulong end)
+        {
+            var context = Context.GetDefault();
+
+            var attr_data_buffer_read = new int[CellVal];
+
+            var subarray = new int[4] { 1, 1, 1, 1 };
+
+            using (var array_read = new Array(context, arrayUri))
+            {
+                array_read.SetOpenTimestampStart(start);
+                array_read.SetOpenTimestampEnd(end);
+
+                array_read.Open(QueryType.Read);
+                var query_read = new Query(context, array_read);
+                query_read.SetLayout(LayoutType.RowMajor);
+                query_read.SetSubarray(subarray);
+                query_read.SetDataBuffer<int>("a", attr_data_buffer_read);
+                query_read.Submit();
+                var status_read = query_read.Status();
+                array_read.Close();
             }
 
+            System.Console.WriteLine($"READ: {string.Join(", ", attr_data_buffer_read)}");
+        }
+
+        public static void Run()
+        {
+            System.Console.WriteLine(arrayUri);
             CreateArray();
+            //CreateArray();
             WriteArray();
+            ConsolidateArray();
             ReadArray();
-            ReadArrayAsync();
+            //ReadArrayAsync();
+            ReadFragments();
         }
     }//class
 }//namespace
--- examples/TileDB.CSharp.Example/Program.cs
+++ examples/TileDB.CSharp.Example/Program.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.IO;
 using TileDB.CSharp;
-namespace TileDB.CSharp.Examples
+namespace Examples
 {
-    static class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("TileDB Core Version: {0}", CoreUtil.GetCoreLibVersion());
-
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "/source/drago-testing-serviceaccount.json");
+            
+            /*
+            using (var readtext = new StreamReader(@"/source/drago-testing-serviceaccount.json"))
+            {
+                while (!readtext.EndOfStream)
+                {
+                    string currentLine = readtext.ReadLine();
+                    Console.WriteLine(currentLine);
+                }
+            }
+            */
             ExampleQuery.Run();
-            ExampleIncompleteQuery.Run();
-            ExampleIncompleteQueryStringDimensions.Run();
-            ExampleWritingDenseGlobal.Run();
-            ExampleWritingSparseGlobal.Run();
+            //ExampleIncompleteQuery.Run();
+            //ExampleIncompleteQueryStringDimensions.Run();
 
-            ExampleFile.RunLocal();
-            // ExampleFile.RunCloud("tiledb_api_token", "tiledb_namespace", "new_cloud_array_name", "s3://bucket/prefix/");
+            //ExampleFile.RunLocal();
+            // ExampleFile.RunCloud("tiledb_api_token", "tiledb_namespace", "cloud_array_name", "s3://bucket/prefix/");
 
-            ExampleGroup.RunLocal();
+            //ExampleGroup.RunLocal();
             // ExampleGroup.RunCloud("tiledb_api_token", "tiledb_namespace", "s3://bucket/prefix");
         }
     }
